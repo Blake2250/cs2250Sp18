f57e0af0a2445227fddc68707baaaba35f0ed5b7
Blake Evans <blakeevans@mail.weber.edu>
2018-01-23 09:51:38 -0700

    initial commit

---
+++ mod3/bodyWeight.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    doble weight, height, bmi;
+        // 1) capture weight in kg
+    printf("190");
+    scanf("%Lf , &weight);
+
+        // 2) captuer the height in meters
+    printf("Enter your height: "
+
+        // 3) calculate your BMI = weight/height**2
+
+        // 4) Display BMI
+
+        
+
+
+        return0; }
---
+++ mod3/char.c
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  char.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/23/2018 08:51:43 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Blake Evans (), blakeevans@mail.weber.edu
+ *   Organization:  wsu
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    char input, dummy;
+    int age;
+    printf("Do you like to code?:")
+        scanf("%c" , &input);
+        printf("1)You entered [%c]\n" , input);
+        printf("Do you like to code?:");
+        scanf("%c" , &input);
+        printf("Do you like to code?:");
+        
+    return 0;
+}
+// Function Definitions
+
+
